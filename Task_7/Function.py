8f812c042f099f102d99103a736c6950bee3615a
Inayat Ullah <anayat2019@namal.edu.pk>
2023-03-26 23:18:32 +0500

    task 7 modified

--- Task_7/Function.py
+++ Task_7/Function.py
@@ -42,7 +42,14 @@ def Docs():
     '''
 print(Docs.__doc__)
 
+# Return in python
 print("---------")
+def Get_Full_Name(first_name, last_name):
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+print(Get_Full_Name('inayat', 'ullah'))
+
+print("------------------")
 # Return in python
 def Squar(num):
     return (num**2)
@@ -58,6 +65,68 @@ def num(n):
             return("Greater then 10")
     else:
         return False
-n = int(input("Enter Any number you want! "))
+n = 11
 print(num(n))
+
 print("---------")
+def number(num,num1):
+    if num > num1:
+        return num
+    else:
+        print("Ho",num1)
+        return num1
+        #print("Ho")
+def Helloo(num2):
+    num3 = number(2,3)
+    sum1 = num2 + num3
+    return sum1
+num2 = 7
+
+print(Helloo(num2))
+print("---------")
+
+# A default function for addition  
+def Adding(a):  
+    # Nested function with second number   
+    def Addition(b):   
+            return a + b # addition of two numbers  
+    return Addition # Result  
+  
+# Taking both number variable as user input  
+a = 4  
+b = 5 
+# Assigning nested adding function to a variable  
+AddVariable = Adding(a)  
+# Using variable as high order function  
+Result = AddVariable(b)  
+# Printing result  
+print("Sum of Two numbers given by you is: ", Result)  
+
+# Use of title built in function 
+print("---------")
+def user(username):
+ print("Hello, " + username.title() + "!")
+user('inaYat')
+
+# Function that retrun My info in dict form
+print("---------")
+def My(first_name, last_name, age=''):
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+musician = My('Inayat', 'Ullah', age=23)
+print(musician)
+
+# Function that return city and Country
+print("---------")
+def city_country(City,Country):
+    Info = City+" , "+Country
+    return Info.title()
+num = city_country("wana","pakistan")
+print(num)
+
+print("---------")
+un = ['iphone case', 'robot pendant', 'dodecahedron']
+while len(un)>0:
+    un = un.pop()
